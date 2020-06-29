df267df53620031efb99c655a5c388e5741c8395
lana <sitdownrightnow.dev@gmail.com>
2020-06-29 13:05:44 +0700

    add mapper method for updateUserCommand

--- src/main/java/com/example/domain/user/infrastructure/mapper/UserMapper.java
+++ src/main/java/com/example/domain/user/infrastructure/mapper/UserMapper.java
@@ -1,10 +1,12 @@
 package com.example.domain.user.infrastructure.mapper;
 
-import com.example.domain.user.core.model.AddNewUserCommand;
+import com.example.domain.user.core.model.CreateUserCommand;
+import com.example.domain.user.core.model.UpdateUserCommand;
 import com.example.domain.user.core.model.User;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "cdi")
 public interface UserMapper {
-    User fromAddNewUserCommand(AddNewUserCommand addNewUserCommand);
+    User fromCreateUserCommand(CreateUserCommand createUserCommand);
+    User fromUpdateUserCommand(UpdateUserCommand updateUserCommand);
 }
